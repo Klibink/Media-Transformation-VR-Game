f78c2d87380eceab9280195333de3bfe42296598
Klibink <35121031+Klibink@users.noreply.github.com>
2018-11-27 18:08:06 +0100

    Add project

---
+++ RandomGeneratedMazeGame/Assets/MyData/controller.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class controller : MonoBehaviour {
+    
+    private bool walking = true;
+    private Vector3 spawnPoint;
+    private AudioSource footsteps;
+    private bool playing = false;
+
+    private bool wasHittingLastFrame = false;
+    
+	// Use this for initialization
+	void Start () {
+
+        spawnPoint = transform.position;
+        footsteps = GetComponent<AudioSource>();
+        footsteps.Play(0);
+        playing = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (walking)
+        {
+            Vector3 lookDir = new Vector3(Camera.main.transform.forward.x,
+                0,Camera.main.transform.forward.z);
+            lookDir.Normalize();
+            transform.position = transform.position + lookDir * 3.0f * Time.deltaTime;
+        }
+
+        if(walking&& !playing)
+        {
+            footsteps.UnPause();
+            playing = true;
+        }
+        else if(!walking && playing)
+        {
+            footsteps.Pause();
+            playing = false;
+        }
+        if(transform.position.y < -10f)
+        {
+            transform.position = spawnPoint;
+        }
+
+
+        float fYRot = Camera.main.transform.forward.y;
+        if (fYRot < -0.4f)
+        {
+            if (!wasHittingLastFrame)
+            {
+                walking = !walking;
+            }
+            wasHittingLastFrame = true;
+        } else
+        {
+            wasHittingLastFrame = false;
+        }
+	}
+}
---
+++ RandomGeneratedMazeGame/Assets/moveWalls.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveWalls : MonoBehaviour {
+
+    private float startMoveWalls = 5f;
+    private bool rotateds=false;
+    private float time = 0f;
+    private Mesh mesh;
+    private float scaleY = 1f;
+    private float scaleX = 1f;
+    private bool direction = true;
+    private Vector3[] originalVertices;
+
+    private bool isFloor = false;
+    private bool isRotated = false;
+
+	// Use this for initialization
+	void Start () {
+        MeshFilter filter = GetComponent<MeshFilter>();
+        isFloor = filter.transform.up.y < 0.7f;
+        if (isFloor)
+        {
+            return;
+        }
+        isRotated = !(filter.transform.right.x < 0.7);
+
+        mesh = GetComponent<MeshFilter>().mesh;
+
+        originalVertices = new Vector3[mesh.vertices.Length];
+        for (int i = 0; i < mesh.vertices.Length; i++)
+        {
+            originalVertices[i] = new Vector3();
+            originalVertices[i].x = mesh.vertices[i].x;
+            originalVertices[i].y = mesh.vertices[i].y;
+            originalVertices[i].z = mesh.vertices[i].z;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isFloor)
+        {
+            return;
+        }
+        time += Time.deltaTime;
+        if(time>= startMoveWalls)
+        {
+            if (direction)
+            {
+                scaleY += Time.deltaTime * 0.3f;
+                scaleX += Time.deltaTime * 0.9f;
+                if (scaleY >= 2f)
+                {
+                    direction = false;
+                    scaleY = 2f;
+                    scaleX = 4f;
+                }
+            }
+            else
+            {
+                scaleY -= Time.deltaTime * 0.3f;
+                scaleX -= Time.deltaTime * 0.9f;
+                if (scaleY <= 1f)
+                {
+                    rotateds = !rotateds;
+                    direction = true;
+                    scaleY = 1f;
+                    scaleX = 1f;
+                }
+            }
+            Vector3[] vertices = new Vector3[mesh.vertices.Length];
+            for (int i = 0; i < mesh.vertices.Length; i++)
+            {
+                vertices[i] = new Vector3();
+
+                vertices[i].x = originalVertices[i].x * 1;
+
+                vertices[i].y = originalVertices[i].y * scaleY;
+                if (vertices[i].y < -0.5f) vertices[i].y = -0.5f;
+                
+
+                if ((rotateds && isRotated) || (!rotateds && !isRotated))
+                {
+                    vertices[i].z = originalVertices[i].z * scaleX;
+                }
+                else
+                {
+                    vertices[i].z = originalVertices[i].z * 1;
+                }
+            }
+            mesh.vertices = vertices;
+            mesh.RecalculateBounds();
+            //mesh.UploadMeshData(false);
+
+        }
+	}
+}
