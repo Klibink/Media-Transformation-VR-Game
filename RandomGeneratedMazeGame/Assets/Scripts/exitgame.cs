dca578c3c9aa89590c418f70d8344098f1dc702c
Klibink <35121031+Klibink@users.noreply.github.com>
2018-11-29 00:33:43 +0100

    tests mit startmenu

---
+++ RandomGeneratedMazeGame/Assets/Scripts/exitgame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitgame : MonoBehaviour {
+
+    public void ClickExit()
+    {
+        Application.Quit();
+        Debug.Log("Spiel beendet");
+    }
+}
