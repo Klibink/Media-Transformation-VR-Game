55dbefc4f9a54f07a814e12b491336f518275310
Klibink <35121031+Klibink@users.noreply.github.com>
2019-02-13 12:21:15 +0100

    edited some scenes

---
+++ RandomGeneratedMazeGame/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetBool("open", true);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
